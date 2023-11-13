7f12bc44225a283eb79ad30e4b36eca57d28e157
Sergio Serra <sergioserra.2003@gmail.com>
2023-11-13 16:36:43 +0000

    Update csv_to_xml

--- src/samples/xml-generation/csv_to_xml_converter.py
+++ src/samples/xml-generation/csv_to_xml_converter.py
@@ -3,9 +3,12 @@ import xml.dom.minidom as md
 import xml.etree.ElementTree as ET
 
 from csv_reader import CSVReader
+from entities.order import Order
+from entities.ship import Ship
+from entities.customer import Customer
+from entities.address import Address
+from entities.segment import Segment
 from entities.country import Country
-from entities.team import Team
-from entities.player import Player
 
 
 class CSVtoXMLConverter:
@@ -14,46 +17,74 @@ class CSVtoXMLConverter:
         self._reader = CSVReader(path)
 
     def to_xml(self):
+        
         # read countries
         countries = self._reader.read_entities(
-            attr="nationality",
-            builder=lambda row: Country(row["nationality"])
+            attr="Country",
+            builder=lambda row: Country(row["Country"])
         )
 
-        # read teams
-        teams = self._reader.read_entities(
-            attr="Current Club",
-            builder=lambda row: Team(row["Current Club"])
+        # read segments
+        segments = self._reader.read_entities(
+            attr="Segment",
+            builder=lambda row: Segment(row["Segment"])
         )
 
-        # read players
-
-        def after_creating_player(player, row):
-            # add the player to the appropriate team
-            teams[row["Current Club"]].add_player(player)
-
-        self._reader.read_entities(
-            attr="full_name",
-            builder=lambda row: Player(
-                name=row["full_name"],
-                age=row["age"],
-                country=countries[row["nationality"]]
-            ),
-            after_create=after_creating_player
+        # read orders
+        orders = self._reader.read_entities(
+            attr="Order ID",
+            builder=lambda row: Order(
+                order_id=row["Order ID"],
+                order_date=row["Order Date"],
+                market=row["Market"],
+                order_priority=row["Order Priority"],
+                ship_info=Ship(
+                    ship_date=row["Ship Date"],
+                    ship_mode=row["Ship Mode"],
+                    shiping_cost=row["Shipping Cost"]
+                )
+            )
         )
+
+        # read customers
+        customers = self._reader.read_entities(
+            attr="Customer ID",
+            builder=lambda row: Customer(
+                customer_id=row["Customer ID"],
+                customer_name=row["Customer Name"],
+                segment=segments[row["Segment"]],
+                address=Address(
+                    city=row["City"],
+                    country=countries[row["Country"]],
+                    postal_code=row["Postal Code"],
+                    state=row["State"]
+                )
+            )
+        )
+
 
         # generate the final xml
-        root_el = ET.Element("Football")
+        root_el = ET.Element("Store")
 
-        teams_el = ET.Element("Teams")
-        for team in teams.values():
-            teams_el.append(team.to_xml())
+        orders_el = ET.Element("Orders")
+        for order in orders.values():
+            orders_el.append(order.to_xml())
+
+        customers_el = ET.Element("Customers")
+        for customer in customers.values():
+            customers_el.append(customer.to_xml())
+
+        segments_el = ET.Element("Segments")
+        for segment in segments.values():
+            segments_el.append(segment.to_xml())
 
         countries_el = ET.Element("Countries")
         for country in countries.values():
             countries_el.append(country.to_xml())
 
-        root_el.append(teams_el)
+        root_el.append(orders_el)
+        root_el.append(customers_el)
+        root_el.append(segments_el)
         root_el.append(countries_el)
 
         return root_el
