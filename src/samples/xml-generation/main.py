28d24559c7ea17b6eca3bee3e131ed8fec05939a
Sergio Serra <sergioserra.2003@gmail.com>
2023-11-21 20:34:29 +0000

    Optimize code

--- src/samples/xml-generation/main.py
+++ src/samples/xml-generation/main.py
@@ -2,8 +2,9 @@ from csv_to_xml_converter import CSVtoXMLConverter
 
 if __name__ == "__main__":
     converter = CSVtoXMLConverter("/data/Global_Superstore2.csv")
-    print(converter.to_xml_str())
+    data = converter.to_xml_str()
+    print(data)
 
     #export to xml file
     with open("/data/Global_Superstore2.xml", "w") as file:
-        file.write(converter.to_xml_str())
+        file.write(data)
