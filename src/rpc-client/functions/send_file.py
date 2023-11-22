714c7ab91311151e018e17bb21dd7eea61abab8d
Sergio Serra <sergioserra.2003@gmail.com>
2023-11-22 20:38:05 +0000

    Validate type of file when send to server

--- src/rpc-client/functions/send_file.py
+++ src/rpc-client/functions/send_file.py
@@ -2,7 +2,10 @@ import xmlrpc.client
 from functions.connect_rpc import connect_rpc
 
 def send_file(file_name):
-    with open(f'/data/{file_name}', 'rb') as handle:
-        binary_data = xmlrpc.client.Binary(handle.read())
-        server = connect_rpc().server
-        return server.send_file(binary_data, file_name)
+    if file_name.endswith('.csv'):
+        with open(f'/data/{file_name}', 'rb') as handle:
+            binary_data = xmlrpc.client.Binary(handle.read())
+            server = connect_rpc().server
+            return server.send_file(binary_data, file_name)
+    else:
+        return "Invalid file format. Only CSV files are supported."
