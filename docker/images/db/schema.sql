e13b7b235fa5c6924c6ca62be8dab6b7b2e40b9d
Sergio Serra <sergioserra.2003@gmail.com>
2023-11-22 16:55:03 +0000

    Update database schema

--- docker/images/db/schema.sql
+++ docker/images/db/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE public.imported_documents (
 	id              serial PRIMARY KEY,
-	file_name       VARCHAR(250) UNIQUE NOT NULL,
+	file_name       VARCHAR(250) NOT NULL,
 	xml             XML NOT NULL,
+	is_deleted      BOOLEAN NOT NULL DEFAULT FALSE,
 	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
-	is_deleted		BOOLEAN NOT NULL DEFAULT FALSE
 );
